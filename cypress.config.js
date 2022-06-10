5786c082925458ac5ee03493d9457ec86807bc4f
rodriguezespinoza0795 <rodriguezespinoza0795@gmail.com>
2022-06-09 22:55:50 -0500

    primer prueba

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  component: {
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+      excludeSpecPattern: ["**/1-getting-started/*.js",
+        "**/2-advanced-examples/*.js"],
+      setupNodeEvents(on, config) {
     },
   },
 });
