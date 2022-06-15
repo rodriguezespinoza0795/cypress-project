f5ad96bc91b2c647972c8ee0f1e2335a56c32c75
rodriguezespinoza0795 <rodriguezespinoza0795@gmail.com>
2022-06-14 22:45:27 -0500

    test status and headers

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,11 @@ module.exports = defineConfig({
   component: {
   },
   e2e: {
-      excludeSpecPattern: ["**/1-getting-started/*.js",
+    baseUrl: 'http://localhost:3000',
+    excludeSpecPattern: ["**/1-getting-started/*.js",
         "**/2-advanced-examples/*.js"],
       setupNodeEvents(on, config) {
     },
+
   },
 });
